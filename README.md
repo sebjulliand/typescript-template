a3f4b4f35fde728eff1e05c3a3a4385a9c463f86
Seb Julliand <sebjulliand@gmail.com>
2024-09-28 15:46:52 +0200

    Repository initialization
    Signed-off-by: Seb Julliand <sebjulliand@gmail.com>

---
+++ README.md
@@ -0,0 +1,9 @@
+# TypeScript + webpack project template
+A simple template for Typescript projects using Webpack as a bundler.
+The project is VS Code ready: it includes a default watch and debug task
+
+## Get started
+- Clone the repository
+- Run `npm i`
+- Open the project in VS Code
+- Press `F5` to debug
---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+//@ts-checks
+'use strict';
+
+const path = require(`path`);
+const webpack = require(`webpack`);
+
+/** @typedef {import('webpack').Configuration} WebpackConfig **/
+
+/**@type WebpackConfig*/
+module.exports = {
+  mode: `none`, // this leaves the source code as close as possible to the original (when packaging we set this to 'production')
+  target: `node`, // extensions run in a node context
+  node: {
+    __dirname: false // leave the __dirname-behaviour intact
+  },
+  context: path.join(__dirname),
+  resolve: {
+    // Add `.ts` as a resolvable extension.
+    extensions: [".ts", ".js"],
+    // Add support for TypeScripts fully qualified ESM imports.
+    extensionAlias: {
+      ".js": [".js", ".ts"],
+      ".cjs": [".cjs", ".cts"],
+      ".mjs": [".mjs", ".mts"]
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.([cm]?ts|tsx)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: `esbuild-loader`
+          }
+        ]
+      }
+    ]
+  },
+  entry: {
+    extension: `./src/index.ts`,
+  },
+  output: {
+    filename: path.join(`index.js`),
+    path: path.join(__dirname, `dist`),
+    library: {
+      "type": "commonjs"
+    }
+  },
+  // yes, really source maps
+  devtool: `source-map`,
+  plugins: [
+    new webpack.IgnorePlugin({ resourceRegExp: /(bufferutil|utf-8-validate)/u })
+  ]
+}; 
